2e1721c95bc6af19ae603c8ae3381a740eb76a8c
lime9903 <lime9903@daum.net>
2022-04-14 03:03:09 +0900

    step3

---
+++ OpenCV_Python/0423.py
@@ -0,0 +1,25 @@
+# 통계적 거리 계산
+import cv2
+import numpy as np
+
+X = np.array([[0, 0,  0,100,100,150, -100,-150],
+              [0,50,-50,  0, 30,100,  -20,-100]], dtype=np.float64)
+X = X.transpose()
+
+cov, mean = cv2.calcCovarMatrix(X, mean=None,
+                                flags=cv2.COVAR_NORMAL + cv2.COVAR_ROWS)
+
+print('mean=', mean)
+print(' cov=', cov)
+
+ret, icov = cv2.invert(cov)
+print('icov=', icov)
+
+v1 = np.array([[0],[0]], dtype=np.float64)
+v2 = np.array([[0],[50]], dtype=np.float64)
+
+dist = cv2.Mahalanobis(v1, v2, icov)
+print('dist=', dist)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
